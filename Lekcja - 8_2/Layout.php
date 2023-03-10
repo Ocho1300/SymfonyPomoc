3cbd7393911d2a22d5552ba47ea97be211766881
Ocho1300 <80366915+Ocho1300@users.noreply.github.com>
2023-03-10 13:30:14 +0100

    Add files via upload

---
+++ Lekcja - 8_2/Layout.php
@@ -0,0 +1,21 @@
+    <div class="mt-5">
+        <div class="row">
+            <div class="col-sm-12 col-lg-4">
+                <div class="card-deck">
+                    <div class="card d-flex justify-content-between">
+                        <div class="imageGallery1">
+                            <a href="/images/hosting/"><img src="/images/hosting/" class="card-img-top" alt=""></a>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Wysłano:</small>
+                            <small class="text-muted">Dodał:</small>
+                            <p>
+                                <a href="#" class="badge badge-danger">Usuń</a>                                    
+                                <a href="#" class="badge badge-primary">Ustaw prywatne</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ assets/js/scripts.js
@@ -0,0 +1,1 @@
+$('.imageGallery1 a').simpleLightbox();
